e0926bcf0125a3231cd2f4870c51ffdb342cb64b
automationhelps <support@automationhelps.com>
2026-04-22 11:05:39 -0500

    Update jury-applicants.js

--- api/jury-applicants.js
+++ api/jury-applicants.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
           {
             field: "tags",
             operator: "contains",
-            value: "Ready for Jury"
+            value: "ready for jury"
           }
         ]
       })
