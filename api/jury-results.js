528d4c4a1b39d248faa6ab4503682c3af847e9ea
automationhelps <support@automationhelps.com>
2026-05-22 10:48:35 -0500

    Update jury-results.js

--- api/jury-results.js
+++ api/jury-results.js
@@ -1,7 +1,10 @@
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
+  return res.status(200).json({
+    hasGhlApiKey: !!process.env.GHL_API_KEY,
+    hasGhlLocationId: !!process.env.GHL_LOCATION_ID,
+    hasGhlJuryFormId: !!process.env.GHL_JURY_FORM_ID
+  });
+}
 
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
